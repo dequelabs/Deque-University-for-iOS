c66e4bdd1a070f91ffe8db764fd3351075c65c9d
Catherine Fisher <catherine.fisher@deque.com>
2014-02-05 15:57:46 -0500

    Initial Commit

---
+++ iosFireEyesInstructions/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iosFireEyesInstructions
+//
+//  Created by Catherine Fisher on 2/5/14.
+//  Copyright (c) 2014 Deque Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DQAppDelegate class]));
+    }
+}
---
+++ iosFireEyesInstructionsTests/iosFireEyesInstructionsTests.m
@@ -0,0 +1,34 @@
+//
+//  iosFireEyesInstructionsTests.m
+//  iosFireEyesInstructionsTests
+//
+//  Created by Catherine Fisher on 2/5/14.
+//  Copyright (c) 2014 Deque Systems. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iosFireEyesInstructionsTests : XCTestCase
+
+@end
+
+@implementation iosFireEyesInstructionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
