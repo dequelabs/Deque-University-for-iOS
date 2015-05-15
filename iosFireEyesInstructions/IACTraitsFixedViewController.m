26e5387830e2db7a93331bcd85ebe873b237e5a7
Jennifer Dailey <jennifer.dailey@deque.com>
2015-05-15 14:09:44 -0400

    IAC-15 #time 4h #comment Finished styling for all demos and introduction

--- iosFireEyesInstructions/IACTraitsFixedViewController.m
+++ iosFireEyesInstructions/IACTraitsFixedViewController.m
@@ -7,6 +7,14 @@
 //
 
 #import "IACTraitsFixedViewController.h"
+#import "DQConstants.h"
+#import "IACUtilities.h"
+
+@interface IACTraitsFixedViewController ()
+
+@property UIColor* backgroundColorView;
+
+@end
 
 @implementation IACTraitsFixedViewController
 
@@ -38,6 +46,9 @@
     [_ImageView setAccessibilityHint:@""]; //Sometimes hints aren't needed, this silences the warning.
     [_ImageView setAccessibilityLabel:NSLocalizedString(@"DOG", nil)];
     [_ImageView setIsAccessibilityElement:YES];
+    
+    self.backgroundColorView = [IACUtilities colorWithHexString:DQ_COLOR_WORLDSPACE_GREEN];
+    self.view.backgroundColor = self.backgroundColorView;
 }
 
 - (void)displayImage:(id)sender {
